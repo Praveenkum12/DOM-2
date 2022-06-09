159c9b048bfe378a965b71144aefee9cfa49fe37
Praveenkum12 <praveenkum1225@gmail.com>
2022-06-10 03:09:18 +0530

    init

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const openModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openingModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closingModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', openingModal);
+}
+
+closeModal.addEventListener('click', closingModal);
+overlay.addEventListener('click', closingModal);
